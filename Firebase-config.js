32d4e7609627ac70ec997d0a196bddb5b6c3d673
kikidoyouluhme <kimzinzile@gmail.com>
2024-10-16 11:43:16 +0200

    New card request added to Firebase db cardRequests table

--- Firebase-config.js
+++ Firebase-config.js
@@ -1,21 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { initializeAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAKKRJpcb-Z8iCeXB_imZ7odikuKgXzX4M",
-    authDomain: "verifid-d076a.firebaseapp.com",
-    projectId: "verifid-d076a",
-    storageBucket: "verifid-d076a.appspot.com",
-    messagingSenderId: "356732996691",
-    appId: "1:356732996691:web:9b659b266337aabcfb4aa6",
-    measurementId: "G-ET4PLN3NQS"
+  apiKey: "AIzaSyAKKRJpcb-Z8iCeXB_imZ7odikuKgXzX4M",
+  authDomain: "verifid-d076a.firebaseapp.com",
+  projectId: "verifid-d076a",
+  storageBucket: "verifid-d076a.appspot.com",
+  messagingSenderId: "356732996691",
+  appId: "1:356732996691:web:9b659b266337aabcfb4aa6",
+  measurementId: "G-ET4PLN3NQS"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = initializeAuth(app);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
--- screens/campus services/RequestNewCard.jsx
+++ screens/campus services/RequestNewCard.jsx
@@ -1,12 +1,125 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, TextInput, Alert, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import { db } from '../../Firebase-config'; 
+import { addDoc, collection } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth'; 
+import { useNavigation } from '@react-navigation/native';
 
 export default function RequestNewCard() {
-    return (
-        <View>
-            <Text>RequestNewCard</Text>
-        </View>
-    );
+  const navigation = useNavigation();
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+  const [student, setStudent] = useState('');
+
+  
+  const validateStudentNumber = (input) => /^[0-9]*$/.test(input);
+
+  const handleSubmit = async () => {
+  
+    if (!name || !surname || !student) {
+      Alert.alert("Incomplete Form", "Please fill in all fields.");
+      return;
+    }
+
+    
+    if (!validateStudentNumber(student)) {
+      Alert.alert("Invalid Student Number", "Student number must contain digits only.");
+      return;
+    }
+
+    try {
+    
+      const auth = getAuth();
+      const user = auth.currentUser;
+
+     
+      await addDoc(collection(db, 'cardRequests'), {
+       
+        Name: name,
+        Surname: surname,
+        Student_No: student,
+      });
+
+      Alert.alert("Request Submitted", `Name: ${name}\nSurname: ${surname}`);
+     // navigation.navigate('Homescreen');
+      
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Error", "Could not save the data.");
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Request For Student Card</Text>
+
+      <TextInput 
+        style={styles.input} 
+        placeholder="Name:" 
+        value={name} 
+        onChangeText={setName} 
+      />
+
+      <TextInput 
+        style={styles.input} 
+        placeholder="Surname:" 
+        value={surname} 
+        onChangeText={setSurname} 
+      />
+
+      <TextInput 
+        style={styles.input} 
+        placeholder="Student number:" 
+        keyboardType="numeric" 
+        value={student} 
+        onChangeText={setStudent} 
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+        <Text style={styles.buttonText}>Submit Request</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    padding: 20,
+    alignItems: 'center',
+    backgroundColor: '#E1EBEE',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    borderRadius: 8,
+    width: '50%',
+    backgroundColor: '#B9D9EB',
+    marginBottom: 80,
+    textAlign: 'center',
+  },
+  input: {
+    height: 40,
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 10,
+    marginBottom: 40,
+    paddingHorizontal: 10,
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 15,
+    marginTop: 50,
+    width: 150,
+    height: 50,
+    backgroundColor: '#007791',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
